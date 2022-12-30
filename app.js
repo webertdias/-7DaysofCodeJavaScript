a95dd6b50dc9b85669b8fe9290c01b2b99e67ee6
Webert dos Reis Dias <wdwebert@gmail.com>
2022-12-30 11:17:13 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+if (numeroUm === stringUm){
+    console.log("As variaveis numeroUm e a stringUm tem o mesmo valor , mas tipos diferentes ");
+}else{
+    console.log("As variaveis  numeroUm e a stringUm não tem o mesmo valor");
+}
+
+if (numeroTrinta === stringTrinta){
+    console.log("As variaveis numeroTrinta e a stringTrinta tem o mesmo valor, mas tipos diferentes");
+}else{
+    console.log("As variaveis numeroTrinta e a stringTrinta não tem o mesmo valor");
+}
+
+if (numeroDez === stringDez){
+    console.log("As variaveis numeroDez e a stringDez tem o mesmo valor, mas tipos diferentes");
+}else{
+    console.log("As vairveis numeroDez e a stringDez não tem o mesmo valor");
+}
